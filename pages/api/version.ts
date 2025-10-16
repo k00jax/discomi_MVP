5b8b4f7cb9a2f4ba744963b75e25ac030e820105
Kyle Fonger <kylefonger@gmail.com>
2025-10-16 12:06:34 -0500

    still not working

--- pages/api/version.ts
+++ pages/api/version.ts
@@ -1,4 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ build: process.env.BUILD_ID || "unknown" });
+  res.status(200).json({
+    build: process.env.BUILD_ID || "unknown",
+    file: __filename,
+    sha: process.env.VERCEL_GIT_COMMIT_SHA || "unknown",
+  });
 }
